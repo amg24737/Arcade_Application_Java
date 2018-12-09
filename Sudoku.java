deaa36a757c529287e1729b8635e7993ba349e4a
amg24737 <43379466+amg24737@users.noreply.github.com>
2018-12-08 22:26:19 -0500

    Update Sudoku.java
    changed basically everything

--- Sudoku.java
+++ Sudoku.java
@@ -34,26 +34,26 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.RowConstraints;
 
+/**
+ * This class provides a sudoku game, providing
+ * six pre-set boards representing three levels of
+ * difficulty, and an evaluate button for the player
+ * to determine whether they have filled in the board
+ * correctly according to the rules of the game.
+ *@author Anna Marie Gann
+ * @author Alexander Coleman
+ */
 public class Sudoku {
 
     protected BorderPane root = new BorderPane();
-    protected Button evaluate = new Button();
-    protected TilePane gameBoard = new TilePane();
-    protected TilePane buttons = new TilePane();
+    protected Button evaluate = new Button("Evaluate");
+    protected GridPane gameBoard = new GridPane();
+    protected TextField[][] textArr = new TextField[9][9];
     
-    protected Button oneB = new Button("1");
-    protected Button twoB = new Button("2");
-    protected Button threeB = new Button("3");
-    protected Button fourB = new Button("4");
-    protected Button fiveB = new Button("5");
-    protected Button sixB = new Button("6");
-    protected Button sevenB = new Button("7");
-    protected Button eightB = new Button("8");
-    protected Button nineB = new Button("9");
-
     protected Button playAgain = new Button("Play again");
-    protected Button clearBoard = new Button("Clear Board");
 
     protected TextField winStatus = new TextField(" ");
     protected Label winLabel = new Label("Win or Lose?");
@@ -68,119 +68,272 @@ public class Sudoku {
 
     protected int x = 0;
     protected int y = 0;
+    protected String zeroString = "0";
+
+    protected char[][] hardOne = {
+	{'0', '0', '7', '0', '0', '0', '4', '0', '0'},
+	{'9', '8', '0', '0', '0', '0', '0', '0', '3'},
+	{'0', '0', '0', '6', '1', '9', '0', '0', '0'},
+	{'0', '0', '0', '5', '7', '2', '0', '0', '0'},
+	{'0', '0', '0', '0', '0', '3', '0', '9', '7'},
+	{'0', '0', '0', '0', '6', '0', '0', '0', '0'},
+	{'0', '7', '0', '0', '0', '0', '8', '0', '0'},
+	{'0', '4', '3', '0', '0', '5', '0', '0', '0'},
+	{'0', '0', '2', '0', '4', '0', '7', '0', '0'},
+    };
     
+    protected char[][] hardTwo =  {
+	{'0', '0', '0', '0', '0', '0', '9', '0', '0'},
+	{'0', '5', '0', '0', '3', '0', '0', '0', '0'},
+	{'0', '1', '2', '0', '0', '8', '0', '0', '0'},
+	{'0', '9', '0', '0', '0', '0', '0', '0', '0'},
+	{'0', '2', '0', '0', '0', '1', '0', '3', '0'},
+	{'0', '0', '0', '5', '0', '2', '0', '0', '1'},
+	{'0', '0', '0', '0', '8', '0', '6', '4', '0'},
+	{'0', '6', '0', '1', '2', '0', '0', '0', '0'},
+	{'0', '0', '7', '9', '0', '6', '0', '0', '5'},
+    };
+    
+    protected char[][] medOne =  {
+	{'7', '0', '4', '0', '0', '0', '0', '0', '0'},
+	{'0', '0', '0', '0', '6', '0', '0', '0', '1'},
+	{'0', '9', '5', '7', '0', '0', '0', '0', '0'},
+	{'4', '6', '0', '9', '3', '0', '0', '0', '0'},
+	{'0', '0', '0', '0', '0', '0', '0', '9', '0'},
+	{'0', '7', '0', '0', '0', '0', '2', '0', '0'},
+	{'0', '0', '0', '5', '0', '2', '3', '0', '0'},
+	{'1', '0', '0', '0', '0', '0', '9', '0', '4'},
+	{'0', '4', '0', '0', '0', '7', '0', '0', '0'},
+    };
+    
+    protected char[][] medTwo =  {
+	{'0', '0', '2', '0', '0', '3', '0', '1', '0'},
+	{'7', '0', '0', '0', '6', '0', '0', '0', '0'},
+	{'0', '9', '0', '0', '0', '4', '3', '0', '0'},
+	{'0', '2', '0', '0', '0', '0', '0', '4', '6'},
+	{'1', '0', '0', '0', '8', '0', '0', '7', '0'},
+	{'0', '0', '0', '5', '0', '0', '2', '0', '0'},
+	{'0', '0', '8', '3', '0', '0', '0', '0', '1'},
+	{'0', '3', '0', '0', '0', '0', '0', '0', '0'},
+	{'0', '7', '0', '0', '0', '9', '0', '0', '0'},
+    };
+    
+    protected char[][] easyOne =  {
+	{'0', '0', '0', '0', '0', '0', '9', '0', '2'},
+	{'4', '5', '0', '9', '0', '0', '1', '7', '0'},
+	{'0', '0', '0', '7', '0', '1', '0', '5', '0'},
+	{'0', '8', '2', '0', '0', '0', '4', '0', '7'},
+	{'6', '0', '1', '3', '2', '7', '8', '0', '5'},
+	{'9', '0', '3', '0', '0', '0', '2', '1', '0'},
+	{'0', '3', '0', '6', '0', '8', '0', '0', '0'},
+	{'0', '9', '4', '0', '0', '5', '0', '6', '8'},
+	{'1', '0', '8', '0', '0', '0', '0', '0', '0'},
+    };
+    
+    protected char[][] easyTwo = {
+	{'3', '0', '0', '0', '0', '2', '6', '8', '0'},
+	{'0', '0', '0', '0', '0', '8', '0', '1', '0'},
+	{'0', '2', '4', '0', '0', '0', '9', '3', '0'},
+	{'7', '0', '0', '4', '5', '6', '0', '0', '0'},
+	{'0', '0', '1', '0', '8', '7', '0', '0', '0'},
+	{'2', '0', '0', '0', '1', '0', '0', '0', '0'},
+	{'0', '0', '0', '0', '6', '0', '1', '0', '0'},
+	{'0', '0', '9', '0', '0', '0', '0', '0', '6'},
+	{'0', '0', '5', '0', '0', '0', '3', '0', '9'},
+    };
+    
+    protected String[][] gameBoardId = {
+	{"00", "01", "02", "03", "04", "05", "06", "07", "08"},
+	{"10", "11", "12", "13", "14", "15", "16", "17", "18"},
+	{"20", "21", "22", "23", "24", "25", "26", "27", "28"},
+	{"30", "31", "32", "33", "34", "35", "36", "37", "38"},
+	{"40", "41", "42", "43", "44", "45", "46", "47", "48"},
+	{"50", "51", "52", "53", "54", "55", "56", "57", "58"},
+	{"60", "61", "62", "63", "64", "65", "66", "67", "68"},
+	{"70", "71", "72", "73", "74", "75", "76", "77", "78"},
+	{"80", "81", "82", "83", "84", "85", "86", "87", "88"},
+    };
+    
+    /**
+     * This method initializes the game board
+     * with one of the pre-set game options. 
+     * It also sets an id for each node and makes
+     * the predetermined nodes uneditable.
+     * @param none
+     */
     public void boardSetUp() {
-	gameBoard.setPrefRows(9);
-	gameBoard.setPrefColumns(9);
+	int numRows = 9;
+	int numCols = 9;
+	gameBoard.setGridLinesVisible(true);
+
+	for (int i = 0; i < numCols; i++) {
+		ColumnConstraints colConst = new ColumnConstraints();
+		colConst.setPercentWidth(45.0/numCols);
+		gameBoard.getColumnConstraints().add(colConst);
+	    }//row constraint
+
+	for (int i = 0; i < numRows; i++) {
+		RowConstraints rowConst = new RowConstraints();
+		rowConst.setPercentHeight(45.0/numRows);
+		gameBoard.getRowConstraints().add(rowConst);
+	    }//row constraint
+
+	for (int rows = 0; rows < 9; rows++) {
+	    for (int cols = 0; cols < 9; cols++) {
+		textArr[rows][cols] = new TextField("0");
+		textArr[rows][cols].setEditable(true);
+		gameBoard.getChildren().add(textArr[rows][cols]);
+	    }//for cols
+	}//for rows
+	
 	for (int row = 0; row < 9; row++) {
 	    for (int col = 0; col < 9; col++) {
 		String rowString = Integer.toString(row);
 		String colString = Integer.toString(col);
 		String j = rowString + colString;
-		Button temp = new Button(j);
-		temp.setId(j);
-		gameBoard.getChildren().add(temp);
+	    	String b = Character.toString((easyOne[row][col]));
+		textArr[row][col].setId(j);
+		textArr[row][col].setText(b);
+		if (!(b.equals(zeroString))) {
+			textArr[row][col].setEditable(false);
+		    }//if
 	    }//for col
-	}//for row
+	}//for row	
 	root.setCenter(gameBoard);
-    }//setUpGameBoard
-
+    }//boardSetUp
+    
+	/**
+	 * This method sets up the visual of the game,
+	 * adding all of the buttons, text fields, and
+	 * menu options that the player will see, and 
+	 * formatting each one. 
+	 * @param none
+	 */
     public void setUpGame() {
-	buttons.getChildren().addAll(oneB, twoB, threeB, fourB, fiveB, sixB, sevenB, eightB, nineB);
-	buttons.setPrefColumns(3);
-	buttons.setPrefRows(3);
-	root.setRight(buttons);	
 	winBox.getChildren().addAll(winLabel, winStatus);
-	root.setLeft(winBox);
+	root.setBottom(winBox);
 		     
-	evaluateBox.getChildren().add(evaluate);
+	evaluateBox.getChildren().addAll(evaluate, playAgain);
 	root.setTop(evaluateBox);
     }//setUpGame
-	    
+
+    /**
+     * This method sets up action events
+     * to change the game board as the 
+     * player adds new values to the board.
+     * @param none
+     */
     public void groupHandler() {
 	gameBoard.setOnMousePressed(event-> {
-		String nodeId = ((Button)event.getSource()).getId();
-		char xInd = nodeId.charAt(0);
-		char yInd = nodeId.charAt(1);
-		x = Character.getNumericValue(xInd);
-		y = Character.getNumericValue(yInd);
-		
-		oneB.setOnAction(k -> {
-			((Button)event.getSource()).setText("1");
-			values[x][y] = 1;
-		    });//button one is clicked
+		TextField ttf = ((TextField)event.getSource());
+		String nodeId = ttf.getId();
 
-		twoB.setOnAction(e -> {
-                        ((Button)event.getSource()).setText("2");
-                        values[x][y] = 2;
-                    });//button two is clicked
-
-		threeB.setOnAction(a -> {
-                        ((Button)event.getSource()).setText("3");
-                        values[x][y] = 3;
-                    });//button three is clicked
-
-		fourB.setOnAction(b -> {
-                        ((Button)event.getSource()).setText("4");
-                        values[x][y] = 4;
-                    });//button four is clicked
-
-		fiveB.setOnAction(c -> {
-                        ((Button)event.getSource()).setText("5");
-                        values[x][y] = 5;
-                    });//button five is clicked
-
-		sixB.setOnAction(d -> {
-                        ((Button)event.getSource()).setText("6");
-                        values[x][y] = 6;
-                    });//button six is clicked
-
-		sevenB.setOnAction(f -> {
-                        ((Button)event.getSource()).setText("7");
-                        values[x][y] = 7;
-                    });//button seven is clicked
-
-		eightB.setOnAction(g -> {
-                        ((Button)event.getSource()).setText("8");
-                        values[x][y] = 8;
-                    });//button eight is clicked
-
-		nineB.setOnAction(h -> {
-                        ((Button)event.getSource()).setText("9");
-                        values[x][y] = 9;
-                    });//button nine is clicked
-		
-	    });//event handler
+		for (int i = 0; i < 9; i++) {
+		    for (int j = 0; j < 9; j++) {
+			if (gameBoardId[i][j].equals(nodeId)) {
+				String nodeString = ttf.getText();
+				char importantC = nodeString.charAt(0);
+				int charInt = Character.getNumericValue(importantC);
+				values[i][j] = charInt;
+			    }//if
+			    }//for j
+		    }//for i
+		}); //eventHandler
     }//groupHandler
-    
+
+
+	    /**
+	     * This method is called when the 
+	     * player clicks the evaluate button. 
+	     * The method evaluates the win status
+	     * of the game at the point and reacts accordingly.
+	     * @param none
+	     */
     public void evaluate() {
 	evaluate.setOnAction(event-> {
-		validBox();
 		String winString = "YOU WIN!";
 		String loseString = "Sorry, this configuration is incorrect";
-
-		if (gameWin == true) {
+		if((validBox() == true) && (validRow() == true) && (validColumn() == true)) {
+		    gameWin = true;
 		    winStatus.setText(winString);
 		}//if
-
+	      
 		else {
 		    winStatus.setText(loseString);
 		}//else
 	    });//lambda
     }//evaluate
-    
-    public void validBox() {
-	;
+
+	    /**
+	     * This method checks whether easy
+	     * three by three sub box only has one of
+	     * each number one through nine, and is a valid
+	     * box according to the game rules. 
+	     * @param none
+	     */
+    public boolean validBox() {
+	for (int i = 0; i < 9; i+=3) {
+	    for (int j = 0; j < 9; j+=3) {
+		for (int pos = 0; pos < 8; pos++) {
+		    for (int posTwo = pos+1; posTwo <9; posTwo++) {
+			if (values[i + pos%3][j + pos/3] == values[i + posTwo%3][j + posTwo/3]) {
+			    return false;
+			}//if
+		    }//posTwo
+		}//for pos
+	    }//for j
+	}//for i
+	return true;
     }//validBox
 
-    public void validRow(){
-	;
+
+	    /**
+	     * This method checks whether or not
+	     * each row of the game board is valid
+	     * according to the rules of the game
+	     * @param none
+	     */
+    public boolean validRow(){
+	for (int r = 0; r < 9; r++) {
+	    for (int c = 0; c < 8; c++) {
+		for (int c2 = c+1; c2 <9; c2++) {
+		    if(values[r][c] == values[r][c2])
+			{
+			    return false;
+			}//if
+		}//for c2
+	    }//for c
+	}//for r
+	return true;
     }//validRow
 
-    public void validColumn() {
-	;
+	    /**
+	     * This method checks to 
+	     * make sure that each column of the player's
+	     * board is valid accodring to game rules.
+	     * @param none
+	     */
+    public boolean validColumn() {
+	for (int c = 0; c < 9; c++) {
+	    for (int r = 0; r < 8; r++) {
+		for (int r2 = r+1; r2 < 9; r2++) {
+		    if(values[r][c] == values[2][c])
+			{
+			    return false;
+			}//if
+		}//for r2
+	    }//for r
+	}//for c
+	return true;
     }//validColumn
 
+	    /**
+	     * This method provides the actual
+	     * game loop and is the only method
+	     * directly called from the arcade
+	     * app class. 
+	     * @param a stage for the game
+	     */
     public void play(Stage sudokuStage) {
 	Scene scene = new Scene(root, 600, 500);
 	boardSetUp();
@@ -190,6 +343,6 @@ public class Sudoku {
 	sudokuStage.sizeToScene();
 	sudokuStage.show();
     }//startGame
-    
+
 }//sudoku
 
