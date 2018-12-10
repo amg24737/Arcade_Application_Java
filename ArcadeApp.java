d371526a5abf6a7282d0f1759fc7093d271d71c5
amg24737 <43379466+amg24737@users.noreply.github.com>
2018-12-10 12:02:19 -0500

    Update ArcadeApp.java
    updated

--- ArcadeApp.java
+++ ArcadeApp.java
@@ -1,4 +1,3 @@
-
 package cs1302.arcade;
 //import statements
 import javafx.scene.Group;
@@ -34,34 +33,53 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.TextArea;
 
 public class ArcadeApp extends Application {
 
+    protected BorderPane root = new BorderPane();
     Random rng = new Random();
     protected HBox gameChoice = new HBox();
     protected Button sudokuButton = new Button("Sudoku: Play now!");
     protected Button snakeButton = new Button("Snake: Play now!");
+    protected TextArea welcome = new TextArea();
 
+    protected VBox text1 = new VBox();
+    protected TextField tf1 = new TextField("Welcome to out arcade!");
+    protected TextField tf2 = new TextField("We have two games for you to play: ");
+    protected TextField tf3 = new TextField("Snake and Sudoku.");
+    protected TextField tf4 = new TextField("Click one of the game buttons to begin.");
+    protected TextField tf5 = new TextField("Happy gaming!");
+    
     public void snake () {
 	snakeButton.setOnAction(evant-> {
-
+		Snake sn = new Snake();
+		Stage snakeStage = new Stage();
+		//		sn.startGame(snakeStage);
 	    });//lambda
     }//snake
     
     public void sudoku() {
 	sudokuButton.setOnAction(event-> {
-
+		Sudoku sud = new Sudoku();
+		Stage startStage = new Stage();
+		sud.play(startStage);
 	    });//lambda
     }//sudoku
 
     public void setUp() {
+	text1.getChildren().addAll(tf1, tf2, tf3, tf4, tf5);
+	root.setTop(text1);
+	
 	gameChoice.getChildren().addAll(snakeButton, sudokuButton);
+
+	root.setCenter(gameChoice);
     }//set up
     
     @Override
     public void start(Stage stage) {
 	
-        Scene scene = new Scene(gameChoice, 640, 480);
+        Scene scene = new Scene(root, 640, 480);
         stage.setTitle("cs1302-arcade!");
 	setUp();
 	sudoku();
@@ -72,8 +90,6 @@ public class ArcadeApp extends Application {
 
 	// the group must request input focus to receive key events
 	// @see https://docs.oracle.com/javase/8/javafx/api/javafx/scene/Node.html#requestFocus--
-
-
     } // start
 
     public static void main(String[] args) {
@@ -89,3 +105,4 @@ public class ArcadeApp extends Application {
     } // main
 
 } // ArcadeApp
+
