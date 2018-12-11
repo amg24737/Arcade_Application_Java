46bac0557fd60beb3f07bebe50d512ba5c0304f2
amg24737 <43379466+amg24737@users.noreply.github.com>
2018-12-10 21:17:16 -0500

    Update ArcadeApp.java
    arcade app update with comments

--- ArcadeApp.java
+++ ArcadeApp.java
@@ -1,5 +1,5 @@
 package cs1302.arcade;
-
+//package statement
 //import statements
 import javafx.scene.Group;
 import javafx.scene.input.KeyCode;
@@ -36,71 +36,117 @@ import javafx.scene.text.Font;
 import javafx.scene.input.MouseEvent;
 import javafx.geometry.Pos;
 
+/**
+ * This class launches an arcade application
+ * which provides the user with two games
+ * to play, snake and sudoku, each with
+ * three levels of difficulty
+ *@extends Application
+ *@author Alexander Coleman
+ *@author Anna Marie Gann
+ */
 public class ArcadeApp extends Application {
-
+    //randomizer
     Random rng = new Random();
+    //fade transitions
     FadeTransition extra1;
     FadeTransition extra2;
     FadeTransition extra3;
+    //storage for game choice which
+    //will be set to the scene
     protected VBox gameChoice = new VBox();
+    //sudoku button
     protected Button sudokuButton = new Button("Sudoku: Play now!");
+    //snake button
     protected Button snakeButton = new Button("Snake: Play now!");
+    //arcade title
     protected Label gameTitle= new Label("ARCADE CORNER");
+    //label for team name
     protected Label teamName= new Label("TEAM NAME");
     protected Label authorNames= new Label("Anna Marie Gann & Alexander Coleman");
-
-
+    //label for author names
+    
+    /**
+     * This method has an event handler
+     * which launches the snake game if
+     * the snake button is chosen
+     * @param none
+     */
     public void snake () {
 	snakeButton.setOnAction(evant-> {
+		//create game object
 		Snake sn = new Snake();
+		//stage for game
 		Stage snakeStage = new Stage();
+		//invoke main game loop with
+		//stage to display the game
 		sn.startGame(snakeStage);
 	    });//lambda
     }//snake
-    
+
+    /**
+     * This method has an event
+     * handler which launches the
+     * sudoku game if that button
+     * is pressed by the player
+     *@param none
+     */
     public void sudoku() {
 	sudokuButton.setOnAction(event-> {
+		//create a game object
 		Sudoku sud = new Sudoku();
+		//stage for game
 		Stage startStage = new Stage();
+		//invoke main game loop with stage
 		sud.play(startStage);
 	    });//lambda
     }//sudoku
 
+    /**
+     * This method sets up the first visual
+     * representation of the app, including
+     * animations, font choices, text choices,
+     * theme fills and the like.
+     *@param none
+     */
     public void setUp() {
 	extra1 = new FadeTransition(Duration.millis(2000), gameTitle);
-	extra1.setFromValue(1.0);
+	extra1.setFromValue(1.0); //format fade
 	extra1.setToValue(0.3);
-	extra1.setCycleCount(4);
+	extra1.setCycleCount(4); //cycle count
 	extra1.setAutoReverse(true);
 	gameTitle.setFont(new Font("Arial", 33));
 	gameTitle.setTextFill(Color.RED);
 	gameTitle.setVisible(false);
 	extra2 = new FadeTransition(Duration.millis(3000), teamName);
-	extra2.setFromValue(1.0);
+	extra2.setFromValue(1.0); //format fade
 	extra2.setToValue(0.3);
 	extra2.setCycleCount(10);
 	extra2.setAutoReverse(true);
-	teamName.setFont(new Font("Arial", 33));
-	teamName.setTextFill(Color.BLACK);
-	teamName.setVisible(false);
+	teamName.setFont(new Font("Arial", 33)); //font
+	teamName.setTextFill(Color.BLACK); //color
+	teamName.setVisible(false); //visibility
 	extra3 = new FadeTransition(Duration.millis(4000), authorNames);
-	extra3.setFromValue(1.0);
+	extra3.setFromValue(1.0); //fade properties
 	extra3.setToValue(0.3);
-	extra3.setCycleCount(7);
+	extra3.setCycleCount(7); //cycle count
 	extra3.setAutoReverse(true);
 	authorNames.setFont(new Font("Arial", 25));
-	authorNames.setTextFill(Color.RED);
-	authorNames.setVisible(false);
+	authorNames.setTextFill(Color.RED); //color
+	authorNames.setVisible(false); //vis
    
-	gameChoice.getChildren().addAll(gameTitle,teamName,authorNames,snakeButton,sudokuButton);
+	gameChoice.getChildren().addAll(gameTitle,teamName,
+					authorNames,snakeButton,sudokuButton);
 	gameChoice.setAlignment(Pos.CENTER);
 	gameChoice.setStyle("-fx-background-color: #f0c21e ;");
     }//set up
     
     @Override
     public void start(Stage stage) {
+	//title
         stage.setTitle("cs1302-arcade!");
-	setUp();
+	setUp(); //set up game
+	//event handler
 	gameChoice.setOnMouseClicked(event-> {
 		gameTitle.setVisible(true);
 		extra1.play();
@@ -110,12 +156,13 @@ public class ArcadeApp extends Application {
 		extra3.play();
 	    });//lambda
 
-	sudoku();
-	snake();
+	sudoku(); //sudoku game
+	snake(); //snake game
+	//game choice container for visual
 	Scene scene = new Scene(gameChoice, 640, 480,Color.BLUE);
 	stage.setScene(scene);
-	stage.sizeToScene();
-        stage.show();
+	stage.sizeToScene(); //format
+        stage.show(); //show stage
 
     } // start
 
