2a55be37adbfdedef9ddaf6c4a4486875a2d33ec
alexanderz80 <45432045+alexanderz80@users.noreply.github.com>
2018-12-11 10:29:56 -0500

    Update Snake.java

--- Snake.java
+++ Snake.java
@@ -36,7 +36,13 @@ import javafx.scene.text.Font;
 
 
 
-
+/**
+ * This class provides a snake game, providing
+ * gridboard layout representing three levels of
+ * difficulty, with key controls that moves the snake
+ * @author Anna Marie Gann
+ * @author Alexander Coleman
+ */
 public class Snake{
 
     //variables
@@ -44,7 +50,6 @@ public class Snake{
     GridPane grid = new GridPane();
     int columns = 26;
     int rows = 26;
-    //boolean going = false;
     Timeline timeline;
     Timeline timeline2;
     Timeline timeline3;
@@ -52,9 +57,9 @@ public class Snake{
     boolean Left=false;
     boolean Up=false;
     boolean Down=false;
+    int points = 676;
     int x = 2;
     int y = 5;
-    int points= 0;
     Rectangle r = new Rectangle();
     Rect dot = new Rect();
     Rectangle rect1;
@@ -70,7 +75,13 @@ public class Snake{
     boolean two = false;
     boolean three = false; 
     FadeTransition ft;
+    HBox display = new HBox();
     
+    /**
+     * Styles r object in shape of head.
+     * Styles fruit object, and sets
+     * position in gridpane
+     */
     public void draw(){
 	r.setWidth(10);
 	r.setHeight(10);
@@ -78,12 +89,18 @@ public class Snake{
 	r.setArcHeight(20);
 
 	fruit.setFill(Color.RED);
-	fruit.setWidth(20);
-	fruit.setHeight(20);
+	fruit.setWidth(15);
+	fruit.setHeight(15);
 	grid.getChildren().add(fruit);
 	grid.setConstraints(fruit, 15, 6);
     }//draw
-   
+
+    /**
+     * Sets up key instructions
+     * game over label and winner label.
+     * FadeTransition settings are made.
+     * Adds winner and game over label to HBox
+     */
     public void instru(){
 	con = new Label("USE W-Up,A-Left,S-Down,D-Right KEYS TO CONTROL SNAKE MOVEMENTS");
 	con.setFont(new Font("Arial", 17));
@@ -91,25 +108,37 @@ public class Snake{
 	over.setFont(new Font("Arial", 30));
 	over.setTextFill(Color.RED);
 	over.setVisible(false);
-	winner = new Label("WINNER!!!");
+	winner = new Label("   WINNER!!!");
 	winner.setFont(new Font("Arial", 30));
+	winner.setVisible(false);
 	ft = new FadeTransition(Duration.millis(3000), over);
 	ft.setFromValue(1.0);
 	ft.setToValue(0.3);
 	ft.setCycleCount(4);
 	ft.setAutoReverse(true);
- 
+	display.getChildren().addAll(over,winner);
     }//instru
 
+    /**
+     * Sets up scoreboard thats used to 
+     * display score from current game
+     * @return scoreB HBox for scoring purposes
+     */
     public HBox scoreBoard(){
 	HBox scoreB = new HBox();
 	Label score = new Label("Score : ");
 	lv = new Label();
-	pt = new Label(Integer.toString(points));
+	pt = new Label();
+	pt.setText("0");
 	scoreB.getChildren().addAll(score,pt,lv);
 	return scoreB;
     }//scoreBoard
 
+     /**
+     * Sets up instructions on how
+     * to choose a level
+     * @return levelB HBox for level purposes
+     */
     public HBox levelBoard(){
 	title = new Label("        SNAKE");
 	title.setFont(new Font("Arial", 23));
@@ -120,11 +149,21 @@ public class Snake{
 	return levelB;
     }//scoreBoard
 
+     /**
+     * UpDates players score
+     */
     public void yourScore(){
 	pt.setText(Integer.toString(dot.getRec().size()+1));
     }//yourScore
     
-
+    /**
+     * Checks to see if shapes have overlap
+     * each other
+     * @param head
+     * @param food
+     * @return true overlapping occurred
+     * @return false no overlap
+     */
     public boolean overlaps(Shape head, Shape food){
 	if(head.getBoundsInParent().intersects(food.getBoundsInParent())){
 	    return true;
@@ -132,6 +171,12 @@ public class Snake{
 	return false;
     }
 
+     /**
+     * Checks to see if snake has collided
+     * with its own body
+     * @return true snake collided
+     * @return false snake safe
+     */
     public boolean collide(){
 	for(int z = 0; z < dot.getRec().size();z++){
 	    if( overlaps(r, dot.getRectangle(z))==true){
@@ -141,14 +186,36 @@ public class Snake{
 	return false;
     }//collide
 
+    /**
+     * Checks to see if you're the winner
+     *@return true game is over
+     *@return false game not over
+     */
+    public boolean done(){
+	if(points == dot.getRec().size()+1){
+	    return true;
+	}
+	return false;
+    }//done
+
+    /**
+     * Randomizes the position of
+     * fruit on gridpane
+     */
     public void appear(){
 	int randNum = (int)((Math.random()*25));
 	int randNum2 = (int)((Math.random()*25));
 	grid.setConstraints(fruit, randNum2, randNum);
     }//appear
-    
+
+     /**
+     *This is timeline loop normal speed
+     * speed for game level 1.
+     * Keeps the snake moving and checks
+     * for collisions and out of bounds
+     */
     public void level(){
-	timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+	timeline = new Timeline(new KeyFrame(Duration.millis(750), event -> {
 		    Platform.runLater(() -> {
 			    /* interact with scene graph */
 			    if(overlaps(r,fruit)){
@@ -160,6 +227,11 @@ public class Snake{
 			       	timeline.pause();
 				over.setVisible(true);
 				ft.play();
+				System.out.println(pt.getText());
+			    }//if
+			    if(done()){
+				timeline.pause();
+				winner.setVisible(true);
 			    }//if
 			});
 		    
@@ -168,6 +240,11 @@ public class Snake{
 			dot.move(x,y);
 			
 			y++;
+			if(y == 26){
+			    timeline.pause();
+			    over.setVisible(true);
+			    ft.play();
+			}
 			grid.setConstraints(r, y, x);
 			
 			//loop through rect array and set constraints with each one
@@ -198,6 +275,11 @@ public class Snake{
 		    if(Down){
 			dot.move(x,y);
 			x++;
+			if(x == 26){
+			    timeline.pause();
+			    over.setVisible(true);
+			    ft.play();
+			}
 			grid.setConstraints(r, y, x);
 			
 			for(int b = 0; b < dot.getRec().size();b++){
@@ -228,6 +310,12 @@ public class Snake{
 	timeline.play();
     }//level
 
+     /**
+     *This is a second timeline loop but faster
+     * speed for game level 2
+     * Keeps the snake moving and checks
+     * for collisions and out of bounds
+     */
     public void level2(){
 	//timeline2
 	timeline2 = new Timeline(new KeyFrame(Duration.millis(500), event -> {
@@ -243,12 +331,21 @@ public class Snake{
 				over.setVisible(true);
 				ft.play();
 			    }//if
+			    if(done()){
+				timeline2.pause();
+				winner.setVisible(true);
+			    }//if
 			});
 		    if(Right){
 			//get positon before updating
 			dot.move(x,y);
 			
 			y++;
+			if(y == 26){
+			    timeline.pause();
+			    over.setVisible(true);
+			    ft.play();
+			}
 			grid.setConstraints(r, y, x);
 			
 			//loop through rect array and set constraints with each one
@@ -279,6 +376,11 @@ public class Snake{
 		    if(Down){
 			dot.move(x,y);
 			x++;
+			if(x == 26){
+			    timeline.pause();
+			    over.setVisible(true);
+			    ft.play();
+			}
 			grid.setConstraints(r, y, x);
 		        
 			//loop through rect array and set constraints with each one
@@ -310,6 +412,12 @@ public class Snake{
 	timeline2.play();
     }//level2
 
+    /**
+     *This is a third timeline loop but faster
+     * speed for game level 3
+     * Keeps the snake moving and checks
+     * for collisions and out of bounds
+     */
      public void level3(){
 	//timeline3
 	 timeline3 = new Timeline(new KeyFrame(Duration.millis(350), event -> {
@@ -325,12 +433,21 @@ public class Snake{
 				over.setVisible(true);
 				ft.play();
 			    }//if
+			    if(done()){
+				timeline3.pause();
+				winner.setVisible(true);
+			    }//if
 			});
 		    if(Right){
 			//get positon before updating
 			dot.move(x,y);
 			
 			y++;
+			if(y == 26){
+			    timeline.pause();
+			    over.setVisible(true);
+			    ft.play();
+			}
 			grid.setConstraints(r, y, x);
 	        
 			//loop through rect array and set constraints with each one
@@ -361,6 +478,11 @@ public class Snake{
 		    if(Down){
 			dot.move(x,y);
 			x++;
+			if(x == 26){
+			    timeline.pause();
+			    over.setVisible(true);
+			    ft.play();
+			}
 			grid.setConstraints(r, y, x);
 	        
 			//loop through rect array and set constraints with each one
@@ -394,9 +516,12 @@ public class Snake{
         timeline3.play();
     }//level3
     
-    //@Override
+    /**
+     * Display gameBoard and Snake object
+     * as well as instructions
+     * @param stage new stage to display
+     */
     public void startGame(Stage stage) {
-       
 	//make grid lines
         for(int i = 0; i < columns; i++) {
             ColumnConstraints column = new ColumnConstraints(20);
@@ -415,6 +540,7 @@ public class Snake{
 	
 	//Controls
         Button controls=new Button();
+	//moves controls off screen
         controls.relocate(-100, 200);
         controls.setOnKeyPressed(e->{
             if(e.getCode()==KeyCode.D){
@@ -441,6 +567,7 @@ public class Snake{
                 Down=true;
                 Up=false;
             }
+	    //sets level 1
 	    if(e.getCode()==KeyCode.E){
 	    	if(two == false && three == false){
 		    level();
@@ -448,6 +575,7 @@ public class Snake{
 		    one = true;
 		}//if
 	    }
+	    //sets level 2
 	    if(e.getCode()==KeyCode.Q){
 		if(one == false && three == false){
 		    level2();
@@ -455,6 +583,7 @@ public class Snake{
 		    two = true;
 		}//if
 	    }
+	    //sets level 3
 	    if(e.getCode()==KeyCode.R){
 		if(two == false && one == false){
 		    level3();
@@ -462,12 +591,12 @@ public class Snake{
 		    three = true;
 		}//if
 	    }
+	    // set new game
 	    if(e.getCode()==KeyCode.N){
 		restart(stage);
 	    }
 	    });
-	
-	
+		
 	//sets rectangle first spot
 	grid.setConstraints(r, 5, 2);
 	for(int a = 0; a < dot.getRec().size();a++){
@@ -475,31 +604,37 @@ public class Snake{
 	    grid.getChildren().add(rect1);
 	    grid.setConstraints(rect1, dot.getY(a), dot.getX(a));
 	}//for
-
-
-
+	
+	//sets up scene
 	grid.getChildren().addAll(r,controls);
-	vbox.getChildren().addAll(levelBoard(),grid,scoreBoard(),con,over);	
+	vbox.getChildren().addAll(levelBoard(),grid,scoreBoard(),con,display);	
 	Scene scene = new Scene(vbox,600, 700, Color.WHITE);
 	//Scene scene = new Scene(vbox,(columns * 40) + 50, (rows * 40) + 50, Color.WHITE);
 	stage.setTitle("cs1302-arcade!");
 	stage.setScene(scene);
 	stage.sizeToScene();
-	stage.show();
-
-	
+	stage.show();	
     } // start
 
+    /**
+     * Calls the reset method for the snake game
+     */
     public void restart(Stage stage){
 	reset();
     }//restart
-    
+
+    /**
+     * Resets the snake game points and position
+     */
     public void reset(){
+	//resets snake
 	for(int a = 0; a < dot.getRec().size();a++){
 	    rect1 = dot.getRectangle(a);
 	    grid.getChildren().remove(rect1);
 	}//for
+	//clear messages
 	over.setVisible(false);
+	winner.setVisible(false);
 	Rect redo = new Rect();
 	dot = redo;
 	pt.setText("0");
@@ -512,6 +647,7 @@ public class Snake{
 		grid.getChildren().add(rect1);
 		grid.setConstraints(rect1, dot.getY(a), dot.getX(a));
 	}//for
+	//looks for which timeline to stop
 	if(one){
 	    timeline.stop();
 	    one = false;
@@ -524,8 +660,5 @@ public class Snake{
 	    timeline3.stop();
 	    three = false;
 	}
-    }//reset
-    
-   
-    
+    }//reset    
 } // Snake
